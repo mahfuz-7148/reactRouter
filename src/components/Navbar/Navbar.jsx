19110de40c65d157625060c86d80a635a111b7eb
“mahfuz-7148” <mrahman7148@gmail.com>
2024-12-08 11:54:31 +0600

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-blue-700 text-white justify-center flex'>
+        <a className='p-5' href="">Home</a>
+        <a className='p-5' href="">Blogs</a>
+        <a className='p-5' href="">About</a>
+        <a className='p-5' href="">Contact</a>
+    </div>
+  )
+}
+
+export default Navbar
